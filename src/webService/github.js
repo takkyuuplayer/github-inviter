a990d3ed95196a428cf2988d0e1aa21b955794b9
takkyuuplayer <takkyuuplayer@gmail.com>
2017-11-28 16:59:20 -0500

    Add webService/github

---
+++ src/webService/github.js
@@ -0,0 +1,61 @@
+import freeze from 'deep-freeze';
+import urijs from 'urijs';
+import rp from 'request-promise';
+import merge from 'lodash/merge';
+import isString from 'lodash/fp/isString';
+
+const AUTHOLIZATION_URL = 'https://github.com/login/oauth/authorize';
+const LOGIN_URL = 'https://github.com/login/oauth/access_token';
+
+const SCOPE = 'user:email';
+
+export default class github {
+  constructor({
+    personalToken = process.env.GH_PERSONAL_TOKEN,
+    clientId = process.env.GH_BASIC_CLIENT_ID,
+    clientSecret = process.env.GH_BASIC_CLIENT_SECRET,
+  } = {}) {
+    this.personalToken = personalToken;
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+
+    freeze(this);
+  }
+
+  signInLink(state) {
+    if (!isString(state)) {
+      throw new Error(`state: ${state} should be string`);
+    }
+    return urijs(AUTHOLIZATION_URL).query({
+      client_id: this.clientId,
+      state,
+      scope: SCOPE,
+    });
+  }
+
+  headers() {
+    return {
+      headers: {
+        'User-Agent': 'Request-Promise',
+        Authorization: `token ${this.personalToken}`,
+      },
+    };
+  }
+
+  createAuthorizationRequest(code) {
+    if (!isString(code)) {
+      throw new Error(`code: ${code} should be string`);
+    }
+    return {
+      method: 'POST',
+      uri: LOGIN_URL,
+      form: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        code,
+      },
+    };
+  }
+
+  sendRequest = request => rp(merge({}, this.headers(), request))
+}
---
+++ src/webService/github.spec.js
@@ -0,0 +1,87 @@
+import http from 'http';
+import assert from 'power-assert';
+import urijs from 'urijs';
+
+import Github from './github';
+
+describe('webService/github', () => {
+  describe('instance', () => {
+    it('should have github id,token from env', () => {
+      const ws = new Github();
+
+      assert.strictEqual(ws.personalToken, process.env.GH_PERSONAL_TOKEN);
+      assert.strictEqual(ws.clientId, process.env.GH_BASIC_CLIENT_ID);
+      assert.strictEqual(ws.clientSecret, process.env.GH_BASIC_CLIENT_SECRET);
+    });
+    it('can set property', () => {
+      const ws = new Github({
+        clientId: 'foobar',
+      });
+
+      assert.strictEqual(ws.personalToken, process.env.GH_PERSONAL_TOKEN);
+      assert.strictEqual(ws.clientId, 'foobar');
+      assert.strictEqual(ws.clientSecret, process.env.GH_BASIC_CLIENT_SECRET);
+    });
+    it('should be freezed', () => {
+      const ws = new Github();
+
+      assert.throws(() => { ws.clientId = 'hige'; });
+    });
+  });
+
+  describe('signInLink', () => {
+    const ws = new Github();
+    const link = ws.signInLink('csrf');
+
+    assert(link instanceof urijs);
+    assert.strictEqual(link.protocol(), 'https');
+    assert.strictEqual(link.host(), 'github.com');
+    assert.strictEqual(link.path(), '/login/oauth/authorize');
+    assert.deepStrictEqual(link.search(true), {
+      client_id: ws.clientId,
+      state: 'csrf',
+      scope: 'user:email',
+    });
+  });
+
+  const ws = new Github();
+
+  describe('createAuthorizationRequest', () => {
+    it('should return request', () => {
+      const req = ws.createAuthorizationRequest('code');
+      assert.deepStrictEqual(req, {
+        method: 'POST',
+        uri: 'https://github.com/login/oauth/access_token',
+        form: {
+          client_id: ws.clientId,
+          client_secret: ws.clientSecret,
+          code: 'code',
+        },
+      });
+    });
+  });
+
+  describe('sendRequest', () => {
+    it('should send request ', async () => {
+      const server = http.createServer((req, res) => {
+        assert.deepStrictEqual(
+          req.headers,
+          {
+            authorization: `token ${ws.personalToken}`,
+            'user-agent': 'Request-Promise',
+            host: '127.0.0.1:15000',
+            connection: 'close',
+          },
+        );
+        res.end('{"mocked":"response"}');
+      });
+      server.listen(15000);
+
+      const json = await ws.sendRequest({ uri: 'http://127.0.0.1:15000', some: 'option' });
+
+      assert.strictEqual(json, '{"mocked":"response"}');
+
+      server.close();
+    });
+  });
+});
